8650004a2179a781aadfd0b44f854c8d836d14a9
Liza Ivanova <happypumpkinka@gmail.com>
2026-06-30 13:46:18 +0300

    Исправила ошибки линетров

--- homework/liza_ivanova/hw5/hw5_2.py
+++ homework/liza_ivanova/hw5/hw5_2.py
@@ -1,9 +1,8 @@
 text = 'результат операции: 42'
-print(int(text[19: ]) + 10)
+print(int(text[19:]) + 10)
 
 text = 'результат операции: 514'
-print(int(text[(text.find(':') + 1): ]) + 10)
+print(int(text[(text.find(':') + 1):]) + 10)
 
 text = 'результат работы программы: 9'
-print(int(text[(text.index(':') + 1): ]) + 10)
-
+print(int(text[(text.index(':') + 1):]) + 10)
