51517f59e9346fdbcc8b2a73bbb53499f1b8dc60
Liza Ivanova <happypumpkinka@gmail.com>
2026-06-30 15:37:09 +0300

    Добавлены два файла по дз номер 6

---
+++ homework/liza_ivanova/hw6/hw6.py
@@ -0,0 +1,14 @@
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at'
+        ' dignissim vitae libero')
+text = text.split()
+new_text = []
+
+for word in text:
+    if word.endswith('.'):
+        new_text.append(word.replace('.', '') + 'ing' + '.')
+    elif word.endswith(','):
+        new_text.append(word.replace(',', '') + 'ing' + ',')
+    else:
+        new_text.append(word + 'ing')
+
+print(' '.join(new_text))
