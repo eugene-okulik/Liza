4a65ce4b099657274cd48deeb86c9a7a4a1a7015
Liza Ivanova <happypumpkinka@gmail.com>
2026-07-03 00:05:23 +0300

    Исправила ошибки линтеров

--- homework/liza_ivanova/hw7/hw7_1.py
+++ homework/liza_ivanova/hw7/hw7_1.py
@@ -3,13 +3,13 @@ words = {"I": 3, "love": 5, "Python": 1, "!": 50}
 
 def number_of_words(dictionary):
     for word, number in dictionary.items():
-     i = 0
-     print()
-     while (i < number):
-         if (i <= number):
+    i = 0
+    print()
+    while (i < number):
+        if (i <= number):
             print(word, end="")
             i += 1
-         else:
+        else:
             break
 
 
