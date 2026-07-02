0a44c17a5eb13e7487d5aec2d71370dc31ec17ea
Liza Ivanova <happypumpkinka@gmail.com>
2026-07-02 23:58:48 +0300

    Исправила ошибки линтеров

--- homework/liza_ivanova/hw7/hw7.py
+++ homework/liza_ivanova/hw7/hw7.py
@@ -4,7 +4,7 @@ user_input = 0
 while (number != user_input):
     user_input = int(input("Введите загаданный номер: "))
     if (number != user_input):
-        while(number != user_input):
+        while (number != user_input):
             user_input = int(input("Попробуйте снова: "))
     else:
         break
