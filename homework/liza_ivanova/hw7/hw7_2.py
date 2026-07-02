8aa05f1bbfec2beaf39f067d63b521140cc2bbf7
Liza Ivanova <happypumpkinka@gmail.com>
2026-07-02 23:50:18 +0300

    Домашнее задание номер 7

---
+++ homework/liza_ivanova/hw7/hw7_2.py
@@ -0,0 +1,7 @@
+def sum_of_numbers(text):
+    print(int(text.split()[-1]) + 10)
+
+sum_of_numbers('результат операции: 42')
+sum_of_numbers('результат операции: 54')
+sum_of_numbers('результат работы программы: 209')
+sum_of_numbers('результат: 2')
