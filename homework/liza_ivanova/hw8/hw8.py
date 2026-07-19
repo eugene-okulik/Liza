defc0ce30a6e32a743aa71e0def4a1dc8322b7de
Liza Ivanova <happypumpkinka@gmail.com>
2026-07-19 21:19:23 +0300

    Исправила ошибки линтеров

--- homework/liza_ivanova/hw8/hw8.py
+++ homework/liza_ivanova/hw8/hw8.py
@@ -1,12 +1,14 @@
 import random
 
+
 def salaryBonusResult():
     salary = int(input("Введите зарплату: "))
     bonus = random.choice([True, False])
-    if bonus == True:
+    if bonus:
         salary += random.randint(0, 30000)
         return salary
     else:
         return salary
+
 
 print(f"${salaryBonusResult()}")
