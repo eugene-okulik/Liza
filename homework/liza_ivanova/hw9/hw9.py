181b9a384d810d3a865bbf099bdbb2625f846bd3
Liza Ivanova <happypumpkinka@gmail.com>
2026-07-20 23:35:14 +0300

    Исправила линтеры

--- homework/liza_ivanova/hw9/hw9.py
+++ homework/liza_ivanova/hw9/hw9.py
@@ -3,4 +3,4 @@ import datetime
 given_date = "Jan 15, 2023 - 12:05:33"
 given_date_pyth_type = datetime.datetime.strptime(given_date, "%b %d, %Y - %H:%M:%S")
 print(f"Месяц из даты: {given_date_pyth_type.strftime("%B")}")
-print(f"Дата в формате из дз: {given_date_pyth_type.strftime("%d.%m.%Y, %H:%M")}")
+print(f"Дата в формате из дз: {given_date_pyth_type.strftime('%d.%m.%Y, %H:%M')}")
